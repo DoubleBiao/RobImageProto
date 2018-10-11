2cfd4980839e01a8ec8ef414a4a3c88c636e0221
xiaotian <xh2332@columbia.edu>
2018-10-11 13:45:51 -0700

    new model

--- imagesender.py
+++ imagesender.py
@@ -1,18 +1,55 @@
 #! /usr/bin/python3
-
+import pickle
+import struct
 import socket
 import numpy as np
 
-class ImageSender:
+class RobProtoClient:
+    """based on TCP(socket):
+    (app)                 |(RobProtoClient)                                |(TCP)
+                          |                                                |
+       cmd,value,(image)<---- dict:{"cmd":cmd,"val":val}                   |
+                          |         <---- pickle chunk + datalen(uint32)   |
+                          |                  <----- chunck              <------- socket send
+                          |                                                |
+    
+     ......
+
+     (app)                |(RobProtoClient)                                |(TCP)
+                          |                                                |
+        ACK(string) ------------>    ACK(4 bytes)                 <------------  socket recv 
+                          |                                                |
+                          |                                                |
+    """
+
     def __init__(self, host, port):
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((host,port))
-    def send_data(self,data):   #send bytes stream
-        assert type(data) == bytes,"data to be sent must be converted to bytes stream"
-        self.socket.send(data)
-    def send_image(self,im):
-        self.send_data(im.tobytes())
+    #def send_data(self,data):   #send bytes stream
+    #    assert type(data) == bytes,"data to be sent must be converted to bytes stream"
+    #    self.socket.send(data)
 
+    #    out = self.socket.recv(4).decode()
+    #    print(out)
+    #def send_image(self,im):
+    #    self.send_data(im.tobytes())
+    def get(self):
+        lenchunk=b""
+        while len(lenchunk) < 4:
+            lenchunk += self.socket.recv(4)
+        datalen = struct.unpack("I",lenchunk)[0]
+        datachunk=b""
+        while len(datachunk) < datalen:  #stop when receive the whole image
+            data = self.socket.recv(8192)
+            datachunk+=data
+
+        datadict = pickle.loads(datachunk)
+        return datadict["cmd"],datadict["val"]
+    def ACK(self):
+        self.socket.send("TEST".encode())
+
+    def stop(self):
+        self.socket.close()
 
     def __del__(self):
         self.socket.close()
@@ -20,6 +57,15 @@ class ImageSender:
 
 if __name__ == "__main__":
     rawimg = np.ones((64,64,3),dtype=np.uint8)
-    image_sender = ImageSender("127.0.0.1", 6666)
-    image_sender.send_image(rawimg)
+    robot = RobProtoClient("127.0.0.1", 6666)
+    
+    for i in range(10):
+        datadict,val = robot.get()
+        ########### do something ################
+        print(datadict)
+        print(val)
+        ##########################################
+        robot.ACK()
+
+    robot.stop()
 
--- imageserver.py
+++ imageserver.py
@@ -4,10 +4,29 @@ from _thread import *
 import socket
 import sys
 import numpy as np
+import struct
+import pickle
 
+class RobProtoServer:
+    def __init__(self,host,port,tot_socket):
+        """based on TCP(socket):
+                      
+         .......
+         (app)                |(RobProtoClient)                                |(TCP)
+                              |                                                |
+           cmd,value,(image) ----> dict:{"cmd":cmd,"val":val}                  |
+                              |         ----> pickle chunk + datalen(uint32)   |
+                              |                  -----> chunck              -------> socket send
+                              |                                                |
+        
+         ......
 
-class ImageServer:
-    def __init__(self,host,port,tot_socket,imagesize):
+         (app)                |(RobProtoClient)                                |(TCP)
+                              |                                                |
+            ACK(string)<----------------    ACK(4 bytes)         <----------------   socket recv 
+                              |                                                |
+                              |                                                |
+        """
         self.list_sock = []
         for i in range(tot_socket):
             s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -16,7 +35,6 @@ class ImageServer:
             s.listen(10)
             self.list_sock.append(s)
             print("[*] server listening on %s %d" %(host, (port+i)))
-        self.imagesize = imagesize
 
 
     def setup(self):
@@ -31,18 +49,29 @@ class ImageServer:
 
 
     def clientthread(self, conn):
-        sbuffer=b""
-        while len(sbuffer) < np.prod(self.imagesize):  #stop when receive the whole image
-            data = conn.recv(8192)
-            sbuffer+=data
-        conn.close()
-        recivedim = np.frombuffer(sbuffer,dtype=np.uint8).reshape(self.imagesize)
-        print(recivedim.shape)
+        cmd = "start"
+        val = [20,30]
+        while True:
+            datadict = {"cmd":cmd,"val":val}
+            datachunk = pickle.dumps(datadict)
+            datalen = len(datachunk)
+            lenchunk = struct.pack("I",datalen)
+            chunk = lenchunk+datachunk
+            try:
+                conn.send(chunk)
+            except BrokenPipeError:
+                print("clinet stop")
+                break
+            ##### do something  ############
+            val[0] = val[0] + 1
+            ############################
+       
+            ACK = conn.recv(4).decode
 
     def __del__(self):
         for s in self.list_sock:
             s.close()
 
 if __name__ == "__main__":
-    server = ImageServer('127.0.0.1',6666,2,(64,64,3))
+    server = RobProtoServer('127.0.0.1',6666,2)
     server.setup()
